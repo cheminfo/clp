5f3fd3d1b4f5c12422b72a9601bd4dbb9e6e5cff
Daniel Kostro <kostro.d@gmail.com>
2015-03-04 13:50:38 +0100

    Add facebook profile example

--- auth/facebook/oauth2/index.js
+++ auth/facebook/oauth2/index.js
@@ -1,5 +1,35 @@
 'use strict';
-
+// Facebook profile example
+//{
+//    id: '739343132829716',
+//        username: undefined,
+//    displayName: 'Daniel Kostro',
+//    name:
+//    {
+//        familyName: 'Kostro',
+//            givenName: 'Daniel',
+//        middleName: undefined
+//    },
+//    gender: 'male',
+//        profileUrl: 'https://www.facebook.com/app_scoped_user_id/739343132829716/',
+//    emails: [ { value: 'kostro.d@gmail.com' } ],
+//    provider: 'facebook',
+//    _raw: '{"id":"739343132829716","email":"kostro.d\\u0040gmail.com","first_name":"Daniel","gender":"male","last_name":"Kostro","link":"https:\\/\\/www.facebook.com\\/app_scoped_user_id\\/739343132829716\\/","locale":"en_US","name":"Daniel Kostro","timezone":1,"updated_time":"2014-03-16T09:40:42+0000","verified":true}',
+//    _json:
+//    {
+//        id: '739343132829716',
+//            email: 'kostro.d@gmail.com',
+//        first_name: 'Daniel',
+//        gender: 'male',
+//        last_name: 'Kostro',
+//        link: 'https://www.facebook.com/app_scoped_user_id/739343132829716/',
+//        locale: 'en_US',
+//        name: 'Daniel Kostro',
+//        timezone: 1,
+//        updated_time: '2014-03-16T09:40:42+0000',
+//        verified: true
+//    }
+//}
 var FacebookStrategy = require('passport-facebook');
 module.exports = {};
 
