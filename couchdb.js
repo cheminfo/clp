9922173cdb3f51be534e4207b10eab4e21044fc0
Daniel Kostro <kostro.d@gmail.com>
2015-03-09 20:12:48 +0100

    Renew session on interval

--- couchdb.js
+++ couchdb.js
@@ -1,28 +1,33 @@
 'use strict';
 var auth;
 
-var exp = module.exports = {};
+var exp  = module.exports = {};
 exp.init = function*(config) {
     var nano = require('nano')(config.couchUrl),
         coNano = require('co-nano')(nano),
-        couchdb = coNano.use(config.couchDatabase);
+        couchdb = coNano.use(config.couchDatabase),
+        sleep = require('co-sleep');
 
     exp.db = couchdb;
-
-    var res = yield coNano.auth(config.couchUsername, config.couchPassword);
-    if(res[0] instanceof Error) {
-        throw res[0];
-        return;
+    while(true) {
+        yield sleep(1000*60*9); // 9 minutes
+        yield doAuth();
     }
+    function *doAuth(){
+        var res = yield coNano.auth(config.couchUsername, config.couchPassword);
+        if(res[0] instanceof Error) {
+            throw res[0];
+            return;
+        }
+        var headers = res[1];
 
-    var body = res[0];
-    var headers = res[1];
-
-    if (headers && headers['set-cookie']) {
-        auth = headers['set-cookie'];
-        var nano = require('nano')({url: config.couchUrl, cookie: auth[0]})
-        coNano = require('co-nano')(nano);
-        exp.db = couchdb = coNano.use(config.couchDatabase);
-        exp.authCookie = auth[0];
+        if (headers && headers['set-cookie']) {
+            auth = headers['set-cookie'];
+            var nano = require('nano')({url: config.couchUrl, cookie: auth[0]});
+            console.log('cookie', auth[0]);
+            coNano = require('co-nano')(nano);
+            exp.db = couchdb = coNano.use(config.couchDatabase);
+            exp.authCookie = auth[0];
+        }
     }
 };
